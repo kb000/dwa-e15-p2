d66ea4021377a2f4b6956630907c20cda11b3cdc
Kevin Burek <git.commit@kb0.org>
2016-09-17 14:24:56 -0400

    Content stubs.

---
+++ site/index.php
@@ -0,0 +1,27 @@
+<?php
+    require('generator.php');
+?><!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Passphrase Generator</title>
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab" />
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+
+    <div class="content-wrapper">
+        <div class="content">
+           Lorem-ipsum-dolorum-s1t-amet
+        </div>
+    </div>
+    <script src="script/jquery-3.1.0.slim.js"></script>
+    <script src="script/bootstrap.js"></script>
+</body>
+
+</html>
