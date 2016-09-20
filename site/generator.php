7114c10853452cb907e4a6f7c78fa417711b492b
Kevin Burek <git.commit@kb0.org>
2016-09-19 20:48:48 -0400

    Random word sequence working.

--- site/generator.php
+++ site/generator.php
@@ -1,21 +1,42 @@
 <?php
 
+    require('wordlist.php');
+
 $DEFAULT_WORD_COUNT = '4';
 
 $KEY_WORD_COUNT = 'words';
 $KEY_INCL_NUMBER = 'useNumber';
 $KEY_INCL_SYMBOL = 'useSymbol';
 
-$SEPARATORS = [' ', '-'];
+$SEPARATOR = '-';
 
-#
-# Safely retrieves or defaults an entry from the $_REQUEST array.
-#
+/**
+ * Safely retrieves or defaults an entry from the $_REQUEST array.
+ * @param string $entryName The key of the entry to retrieve.
+ * @param string $defaultValue: The value to return if the entry is not present in $_REQUEST;
+ * @return string The entry from $_REQUEST, if present, or the $defaultValue.
+ */
 function getRequestEntry($entryName, $defaultValue = NULL) {
     if(isset($_REQUEST[$entryName])) {
         return $_REQUEST[$entryName];
     }
     return $defaultValue;
+}
+
+/**
+ * Gets an array of random words.
+ * @param int $count The number of words to retrieve.
+ * @return string[$count] Randomly fetched english words. 
+ */
+function getRandomWordAray($count) {
+    global $WORDLIST;
+    $selected = [];
+    for($i=0; $i<$count; $i++) {
+        $j = rand(1, count($WORDLIST)) - 1;
+        $selected[$i] = $WORDLIST[$j];
+    }
+
+    return $selected;
 }
 
 # Retrieve and parse wordCount.
@@ -25,13 +46,21 @@ if(!($wordCount > 0)) { # NOTE: !(NAN > 0) ==> !(false) ==> true, don't change t
     $GeneratorErrors.Add("Invalid word count specified. Using $DEFAULT_WORD_COUNT.");
 }
 
-# TODO: retrieve and parse booleans.
+# Retrieve and parse includes.
+$includeNumber = (boolean) getRequestEntry($KEY_INCL_NUMBER, FALSE);
+$includeSymbol = (boolean) getRequestEntry($KEY_INCL_SYMBOL, FALSE);
 
-# Static stubs
-$generatedPassword = "this-is-an-example";
-$generatedWordCount = 4;
-$generatedDidIncludeNumber = false;
-$generatedDidIncludeSymbol = false;
+$wordSequence = getRandomWordAray($wordCount);
+
+if(isset($_REQUEST['debug']) && $_REQUEST['debug'] == 'generator.php') {
+    print_r($wordSequence);
+}
+
+# Variables to include in output.
+$generatedPassword = implode($SEPARATOR, $wordSequence);
+$generatedWordCount = $wordCount;
+$generatedDidIncludeNumber = $includeNumber;
+$generatedDidIncludeSymbol = $includeSymbol;
 
 
 function makeCheckboxCheck($checkedBoolean) {
