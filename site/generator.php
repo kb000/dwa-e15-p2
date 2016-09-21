da950cbfbfa4a20c16681713a0e55c804dc22bd0
Kevin Burek <git.commit@kb0.org>
2016-09-21 01:23:58 -0400

    Append number and symbol, track errors.

--- site/generator.php
+++ site/generator.php
@@ -3,6 +3,7 @@
     require('wordlist.php');
 
 $DEFAULT_WORD_COUNT = '4';
+$MAX_WORD_COUNT = '16';
 
 $KEY_WORD_COUNT = 'words';
 $KEY_INCL_NUMBER = 'useNumber';
@@ -39,11 +40,46 @@ function getRandomWordAray($count) {
     return $selected;
 }
 
+function getRandomNumber() {
+    return rand(0,99);
+}
+
+function getRandomSymbol() {
+    $SYMBOLS = ['!','@','#','$','?','.'];
+    return $SYMBOLS[rand(0, count($SYMBOLS) -1)];
+}
+
+class GeneratorErrors {
+    private $messages = [];
+    /**
+     * Adds a message to be reported back to the user.
+     */
+    public function add($message) {
+        array_push($this->messages, $message);
+    }
+
+    /**
+     * Gets the HTML representation of the error messages.
+     */
+    public function getHtml() {
+        # TODO: Build bootstrap error panels.
+        if(isset($this->messages) && count($this->messages) > 0) {
+            print_r($this->messages);
+        }
+    }
+}
+
+$generatorErrors = new GeneratorErrors();
+print_r($generatorErrors);
+
 # Retrieve and parse wordCount.
 $wordCount = getRequestEntry($KEY_WORD_COUNT, $DEFAULT_WORD_COUNT);
-if(!($wordCount > 0)) { # NOTE: !(NAN > 0) ==> !(false) ==> true, don't change this!
+if (!($wordCount > 0)) { # NOTE: !(NAN > 0) ==> !(false) ==> true, don't change this!
     $wordCount = $DEFAULT_WORD_COUNT;
-    $GeneratorErrors.Add("Invalid word count specified. Using $DEFAULT_WORD_COUNT.");
+    $generatorErrors->add("Invalid word count specified. Using $DEFAULT_WORD_COUNT.");
+} elseif ($wordCount > $MAX_WORD_COUNT) {
+    $wordCount = $MAX_WORD_COUNT;
+    $generatorErrors->add("Word count too large. Using $MAX_WORD_COUNT.");
 }
 
 # Retrieve and parse includes.
@@ -52,17 +88,33 @@ $includeSymbol = (boolean) getRequestEntry($KEY_INCL_SYMBOL, FALSE);
 
 $wordSequence = getRandomWordAray($wordCount);
 
-if(isset($_REQUEST['debug']) && $_REQUEST['debug'] == 'generator.php') {
+if (isset($_REQUEST['debug']) && $_REQUEST['debug'] == 'generator.php') {
     print_r($wordSequence);
 }
 
+if ($includeNumber) {
+    $wordSequence[0] .= getRandomNumber();
+}
+
+$appendSymbol = '';
+if ($includeSymbol) {
+    $appendSymbol = getRandomSymbol();
+}
+
+if (isset($_REQUEST['debug']) && $_REQUEST['debug'] == 'generator.php') {
+    print_r(getRandomSymbol());
+}
+
 # Variables to include in output.
-$generatedPassword = implode($SEPARATOR, $wordSequence);
+$generatedPassword = implode($SEPARATOR, $wordSequence) . $appendSymbol;
 $generatedWordCount = $wordCount;
 $generatedDidIncludeNumber = $includeNumber;
 $generatedDidIncludeSymbol = $includeSymbol;
 
 
+/**
+ * Utility function to make a checkbox checked string on true.
+ */
 function makeCheckboxCheck($checkedBoolean) {
     if ($checkedBoolean) {
         return 'checked="checked"';
